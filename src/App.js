1b347a4841a335dfbeace244b1c6824cedf5a199
fpsbernardo <fpsbernardo@gmail.com>
2018-04-18 10:41:33 +0300

    Removed marketplace release date Removed timeline dates

--- src/App.js
+++ src/App.js
@@ -14,37 +14,30 @@ import {
   Top,
   WrappedContent,
 } from './ui';
-import moon from './img/moon.png';
-import rocket from './img/rocket.png';
+import chamelion from './img/chameleon.png';
 
 const Content = styled.div`
   background-image: linear-gradient(to bottom, white, black);
 `;
 
-const StyledContent = WrappedContent.extend`
-  background: url(${moon}) no-repeat -160px 20px;
-  background-color: inherit;
-  background-size: cover;
-  padding-bottom: 0;
+const PaddingTop = styled.div`
+  padding-top: 5%;
 `;
 
-const RocketContainer = styled.div`
-  height: 100%;
-  width: 100%;
-  margin: auto;
-  display: flex;
-  flex-direction: column;
-  justify-content: flex-end;
-  align-items: center;
-  & img {
-    max-height: 60%;
-  }
+const StyledContent = WrappedContent.extend`
+  background: url(${chamelion}) no-repeat -160px 20px;
+  background-color: white;
+  background-position: 70% 125%;
+  background-size: 165%;
+  padding-bottom: 0;
+  padding-top: 15%;
 `;
 
 const Buttons = styled.div`
   display: flex;
   flex-direction: row;
   justify-content: space-between;
+  padding-top: 5%;
 `;
 
 type Props = {
@@ -59,10 +52,13 @@ export const App = ({ authenticated }: Props) => {
     <Content>
       <StyledContent>
         <Top>
-          <Heading center>Change makes money simple</Heading>
-          <Paragraph center>
-            Get started with a free digital wallet. It’s easy!
-          </Paragraph>
+          <Heading center>Buy bitcoin with Euro</Heading>
+          <PaddingTop>
+            <Paragraph center>
+              Who said bitcoin was complicated? Purchase your first virtual
+              currencies via bank transfer.
+            </Paragraph>
+          </PaddingTop>
           <Buttons>
             <Link to="/signup">
               <PrimaryButton inline>Sign up</PrimaryButton>
@@ -72,9 +68,6 @@ export const App = ({ authenticated }: Props) => {
             </Link>
           </Buttons>
         </Top>
-        <RocketContainer>
-          <img src={rocket} alt="Rocket" />
-        </RocketContainer>
       </StyledContent>
     </Content>
   );
